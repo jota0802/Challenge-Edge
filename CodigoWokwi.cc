61fc169da5c60ac6a857d21ce389f7681ed9da69
Jota <jvfranco08@gmail.com>
2024-06-17 12:38:51 -0300

    corrigindo codigo

--- CodigoWokwi.cc
+++ CodigoWokwi.cc
@@ -1,18 +1,23 @@
 #include <LiquidCrystal.h>
 
-// Inicializa a biblioteca de LCD com os pinos usados no Arduino
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+// Define os pinos as serem usados
 const int ledVermelho = 2;
 const int ledAmarelo = 3;
 const int ledVerde = 4;
 const int pinoBotao = 5;
+#define RS 7
+#define E 6
+#define D4 12
+#define D5 10
+#define D6 9
+#define D7 8
+#define LDR A0
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7); // Cria uma instância do LCD
 
 unsigned long tempoInicial;
 unsigned long tempoDeReacao;
 bool esperarPressionar = false;
 
-// Custom characters for progress bar
 byte customChar[8] = {
   B11111,
   B11111,
@@ -44,9 +49,9 @@ void loop() {
   if (esperarPressionar && digitalRead(pinoBotao) == LOW) {
     tempoDeReacao = millis() - tempoInicial;
     lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Tempo de Reacao:");
-    lcd.setCursor(0, 1);
+    lcd.setCursor(4, 0);
+    lcd.print("Tempo:");
+    lcd.setCursor(4, 1);
     lcd.print(tempoDeReacao);
     lcd.print(" ms");
     esperarPressionar = false;
@@ -58,9 +63,9 @@ void loop() {
 
 void telaInicial() {
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(1, 0);
   lcd.print("Teste de Reacao");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(1, 1);
   lcd.print("Formula E");
 
   // Animação de barra de progresso
@@ -80,9 +85,9 @@ void iniciarSequencia() {
   // Contagem regressiva com LEDs, LCD e barras de progresso
   digitalWrite(ledVermelho, HIGH);
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(5, 0);
   lcd.print("Aguarde:");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(5, 1);
   lcd.print("3 ");
   exibirBarraProgresso(0);
   delay(1000);
@@ -90,18 +95,18 @@ void iniciarSequencia() {
   digitalWrite(ledVermelho, LOW);
   digitalWrite(ledAmarelo, HIGH);
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(5, 0);
   lcd.print("Aguarde:");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(5, 1);
   lcd.print("2 ");
   exibirBarraProgresso(1);
   delay(1000);
   
   digitalWrite(ledAmarelo, LOW);
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(5, 0);
   lcd.print("Aguarde:");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(5, 1);
   lcd.print("1 ");
   exibirBarraProgresso(2);
   delay(1000);
@@ -109,15 +114,15 @@ void iniciarSequencia() {
   // Início do teste de reação após a contagem regressiva
   digitalWrite(ledVerde, HIGH);
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Pressione o botao!");
+  lcd.setCursor(3, 0);
+  lcd.print("Pressione!");
   tempoInicial = millis();
   esperarPressionar = true;
 }
 
 void exibirBarraProgresso(int etapa) {
   for (int i = 0; i <= etapa; i++) {
-    lcd.setCursor(2 + i, 1);
+    lcd.setCursor(7 + i, 1);
     lcd.write(byte(0));
   }
 }
